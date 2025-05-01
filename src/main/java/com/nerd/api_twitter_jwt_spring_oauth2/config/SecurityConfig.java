7e2d16817bf978ee033ef55623497acfef6df469
Igor <freitascasteloigor@gmail.com>
2025-05-01 00:06:58 -0300

    private and public keys  created

--- src/main/java/com/nerd/api_twitter_jwt_spring_oauth2/config/SecurityConfig.java
+++ src/main/java/com/nerd/api_twitter_jwt_spring_oauth2/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.nerd.api_twitter_jwt_spring_oauth2.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,9 +10,19 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
+
 
     @Bean
     private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
